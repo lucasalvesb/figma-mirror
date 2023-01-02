0aa06c624f7b58903e392eecf6685e8f20ddbb6a
Lucas Alves <lucasalves94@gmail.com>
2023-01-02 00:19:36 -0300

    making button appear only when textarea focused

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,18 +33,16 @@ export function Post() {
         </p>
 
         <p>
-          <a href="">#frontend</a>{' '}
-          <a href="">#development</a>
+          <a href="">#frontend</a> <a href="">#development</a>
         </p>
-
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback!</strong>
-        <textarea 
-          placeholder="Deixe um comentário"
-        />
-        <button type="submit">Publicar</button>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
       </form>
     </article>
   )
