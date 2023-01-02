ab380db5e168e12f043618a945a7462b42ddb714
Lucas Alves <lucasalves94@gmail.com>
2023-01-02 00:31:43 -0300

    basic skeleton of comment section

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://github.com/lucasalvesb.png"
+        alt=""
+      ></img>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Alves</strong>
+              <time
+                title="11 de Maio às 08:13"
+                dateTime="2022-05-11 08:13:40"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom, Lucão! Parabéns!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
