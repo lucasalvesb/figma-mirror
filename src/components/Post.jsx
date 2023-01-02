8367c5277129809d87c7c5b2eb9e3a75a3331ebd
Lucas Alves <lucasalves94@gmail.com>
2023-01-01 23:30:40 -0300

    first commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/lucasalvesb.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Lucas Alves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time
+          title="11 de Maio às 08:13"
+          dateTime="2022-05-11 08:13:40"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p> Hey guys! </p>
+
+        <p> I've just finished another task! </p>
+
+        <p> Isn't that cool? </p>
+
+        <p>
+          <a href="">https://github.com/lucasalvesb</a>
+        </p>
+
+        <p>
+          <a href="">#frontend</a>{' '}
+          <a href="">#development</a>
+        </p>
+
+      </div>
+    </article>
+  )
+}
