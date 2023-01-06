1d267ff1e7c9f8fd3dce11a4ebcd7e243883fb4a
Lucas Alves <lucasalves94@gmail.com>
2023-01-06 16:13:17 -0300

    responsiveness to the font size added

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,11 @@
 import styles from './Avatar.module.css'
 
 export function Avatar({ hasBorder = true, src }) {
-  const hasBorder = props.hasBorder  != false;
 
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={props.src}
+      src={src}
     />
   )
 }
